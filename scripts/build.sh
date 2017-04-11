0839846da36de51765b9be30bde775eb181bd572
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2017-04-11 11:45:42 +0200

    Update build script for netcoreapp1.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 dotnet restore
 dotnet build src/Hellion.Core/ -f netstandard1.6
 dotnet build src/Hellion.Database/ -f netstandard1.6
-dotnet build src/Hellion.Login/ -f netcoreapp1.0
-dotnet build src/Hellion.Cluster/ -f netcoreapp1.0
-dotnet build src/Hellion.World/ -f netcoreapp1.0
+dotnet build src/Hellion.Login/ -f netcoreapp1.1
+dotnet build src/Hellion.Cluster/ -f netcoreapp1.1
+dotnet build src/Hellion.World/ -f netcoreapp1.1
