ea34af494df93eed7fe8812f3a6df7741b33afbd
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2017-04-11 11:36:35 +0200

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 dotnet restore
-dotnet build src/Hellion.Core/project.json -f netstandard1.6
-dotnet build src/Hellion.Database/project.json -f netstandard1.6
-dotnet build src/Hellion.Login/project.json -f netcoreapp1.0
-dotnet build src/Hellion.Cluster/project.json -f netcoreapp1.0
-dotnet build src/Hellion.World/project.json -f netcoreapp1.0
+dotnet build src/Hellion.Core/ -f netstandard1.6
+dotnet build src/Hellion.Database/ -f netstandard1.6
+dotnet build src/Hellion.Login/ -f netcoreapp1.0
+dotnet build src/Hellion.Cluster/ -f netcoreapp1.0
+dotnet build src/Hellion.World/ -f netcoreapp1.0
